5fac37c649846c755827ff61281fa89ec037ed88
mucyo-7 <d.mucyo1@alustudent.com>
2026-04-03 09:07:57 -0700

    inheritance

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Inherits BaseGeometry class"""
+"""Module that defines a Square class that inherits from Rectangle"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle."""
+    """A Square class that inherits from Rectangle"""
 
     def __init__(self, size):
-        """Initializes data."""
-
+        """Initialize square with size"""
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
-    def __str__(self):
-        """Returns [Square] <width>/<height>."""
-        return str("[Square] {}/{}".format(self.__size, self.__size))
-
     def area(self):
-        """Area of Square"""
+        """Returns the area of the square"""
+        return self.__size * self.__size
 
-        return self.__size ** 2
+    def __str__(self):
+        """Returns string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,1 @@
-kevine
+ kevine
