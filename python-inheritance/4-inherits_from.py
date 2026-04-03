9e5e9d4b65fbac489cf29de334e9cad189c6ea4b
mucyo-7 <d.mucyo1@alustudent.com>
2026-04-03 23:22:44 +0200

    Fix 4-inherits_from.py docstring and PEP8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Checks object class"""
+"""Module that defines inherits_from function."""
 
 
-def is_kind_of_class(obj, a_class):
-    """Checks object class
+def inherits_from(obj, a_class):
+    """Return True if obj is instance of a class that inherited from a_class.
+
     Args:
-        - obj: object to class
-        - a_class: class to check
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class, False otherwise.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
